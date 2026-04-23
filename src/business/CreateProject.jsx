a1209371866cec9440418557afc7da74bcbdaba4
boot2boot2022-website <boot2boot2022@gmail.com>
2026-04-23 12:32:16 +0530

    createproject changes

--- src/business/CreateProject.jsx
+++ src/business/CreateProject.jsx
@@ -10,7 +10,7 @@ export default function CreateProject() {
     ageTo: 63,
     gender: "All",
     loi: 90,
-    cpi:"",
+   cpi: req.body.cpi ? Number(req.body.cpi) : 0,
     incidence: 90,
     timeline: 90,
     openEnded: 2,
