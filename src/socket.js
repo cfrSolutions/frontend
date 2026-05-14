28199db90db4ebddf5e66b4032f6463c7d9f044a
boot2boot2022-website <boot2boot2022@gmail.com>
2026-05-14 13:27:40 +0530

    socket

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,8 @@
-import {io} from "socket.io-client";
+import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_API_URL,{
-    withCredentials: true,
+const socket = io(import.meta.env.VITE_API_URL, {
+  transports: ["websocket"],
+  withCredentials: true,
 });
 
 export default socket;
