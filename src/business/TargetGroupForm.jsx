aafe0124372a9ecdb1e6cef22b36f4166e2ca2cd
boot2boot2022-website <boot2boot2022@gmail.com>
2026-06-10 11:25:18 +0530

    TargetGroupForm

--- src/business/TargetGroupForm.jsx
+++ src/business/TargetGroupForm.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { formatInTimeZone, fromZonedTime } from "date-fns-tz";
+import countryLanguage from "country-language";
 import ct from "countries-and-timezones";
 import {
   Briefcase,
@@ -64,7 +65,13 @@ export default function TargetGroupForm() {
   const timezones = Intl.supportedValuesOf("timeZone");
   const [marketTimezone, setMarketTimezone] = useState("");
   const { projectId, targetGroupId } = useParams();
- 
+ const languages =
+  countryLanguage.getCountryLanguages("IN");
+
+  const langs =
+  countryLanguage
+    .getCountryLanguages(form.market)
+    ?.map(l => l.name[0]) || [];
 //   const countryTimezones = {
 //   US: "America/Chicago",
 //   IN: "Asia/Kolkata",
@@ -78,6 +85,7 @@ export default function TargetGroupForm() {
   const [form, setForm] = useState({
     sector: "",
     market: "",
+    language: "",
     targetCompletes: 90,
     ageFrom: 18,
     ageTo: 63,
